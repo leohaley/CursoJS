3c49fcc469d199335401addcfebd866e5acbbf17
leohaley <yogutee@gmail.com>
2023-01-14 10:15:59 -0300

    att

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,8 @@
-var idade = 22
-if(idade < 18){
-    console.log('Menor de idade')
-}else{
-    console.log('Maior de idade')
+var idade = 16
+if (idade < 16){
+    console.log('Não vota')
+}else if (idade < 18){
+    console.log('Voto opcional')
+}else if (idade >= 18){
+    console.log('Voto obrigatorio')
 }
